ecd797b2a08ccae77a8f00172029fae4835b3b4f
Germanxzx0 <100843640+Germanxzx0@users.noreply.github.com>
2022-05-26 20:31:26 +0300

    dummy, training area update

--- My project/Assets/AScirpts/OpenWhenColide.cs
+++ My project/Assets/AScirpts/OpenWhenColide.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OpenWhenColide : MonoBehaviour
 {
-    public bool move = false;
+    public bool move = true;
     Vector3 originalPos;
 
     // Start is called before the first frame update
@@ -18,21 +18,25 @@ public class OpenWhenColide : MonoBehaviour
     {
         if (!move)
         {
-            transform.position = originalPos;
+            if (transform.position.y > 0.15)
+                transform.position -= Vector3.up * 2 * Time.deltaTime;
         }
 
         if (move)
         {
-            Vector3 gateUp = new Vector3(transform.position.x, 2, transform.position.z);
-            transform.position = gateUp;
-            //transform.position =  gateUp * Time.deltaTime * 0.1f;
+            //Vector3 gateUp = new Vector3(transform.position.x, 2, transform.position.z);
+            //transform.position = gateUp;
+            if (transform.position.y < 2)
+                transform.position += Vector3.up * 2 * Time.deltaTime;
         }
         
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
+        {
             move = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
