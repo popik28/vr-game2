afe8317ced212cf518777c4f28b94514acb4beae
Germanxzx0 <100843640+Germanxzx0@users.noreply.github.com>
2022-06-07 21:16:47 +0300

    stupid is stupid does

--- My project/Assets/canPass.cs
+++ My project/Assets/canPass.cs
@@ -8,29 +8,30 @@ public class canPass : MonoBehaviour
     public GameObject playerController;
 
     private Grabber [] playerGrabber;
+    GameObject kid;
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerGrabber = playerController.GetComponentsInChildren<Grabber>();
+        kid = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-            if (playerGrabber[0].ariel || playerGrabber[1].ariel)
-            {
-                GetComponent<BoxCollider>().enabled = true;
-                transform.GetChild(0).gameObject.SetActive(true);
-            }
+        if (playerGrabber[0].ariel || playerGrabber[1].ariel)
+        {
+            GetComponent<BoxCollider>().enabled = true;
+            kid.SetActive(true) ;
+        }
 
-            if (!playerGrabber[0].ariel && !playerGrabber[1].ariel)
-            {
-                GetComponent<BoxCollider>().enabled = false;
-                transform.GetChild(0).gameObject.SetActive(true);
-            }
-
+        if (!playerGrabber[0].ariel && !playerGrabber[1].ariel)
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            kid.SetActive(false);
+        }
     }
 }
 
