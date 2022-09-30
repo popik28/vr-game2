ad4a9b2075590ef196519c7e8840b7633989ae29
Germanxzx0 <100843640+Germanxzx0@users.noreply.github.com>
2022-09-30 23:21:08 +0300

    3rd NPC, Credits

--- My project/Assets/AScripts/PlayerObjectiveTracker.cs
+++ My project/Assets/AScripts/PlayerObjectiveTracker.cs
@@ -7,13 +7,15 @@ public class PlayerObjectiveTracker : MonoBehaviour
     //Script exists to track ongoing player objectives, completion and new item/area unlocks.
     public bool hasClimbingGear; //Required to climb the castle wall.
     // Start is called before the first frame update
-    public GameObject box;
+    public GameObject ClimbCollider;
+    public GameObject NpcHandJet;
     public GameObject NpcClimb;
     public GameObject NpcStart;
 
     void Start()
     {
         hasClimbingGear = false;
+        NpcHandJet.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,11 +23,21 @@ public class PlayerObjectiveTracker : MonoBehaviour
     {
         if (hasClimbingGear)
         {
-            Destroy(box);
+            Destroy(ClimbCollider);
             NpcClimb.SetActive(true);
             NpcStart.SetActive(false);
 
         }
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "NPC_3_Activation") 
+        {
+            hasClimbingGear = false;
+            NpcHandJet.SetActive(true);
+            NpcClimb.SetActive(false);
+        }
     }
 }
