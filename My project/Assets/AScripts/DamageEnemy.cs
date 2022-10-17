391b052ce1f84c12f59878284855cd713012a185
popik28 <arielcohen288@gmail.com>
2022-10-17 06:20:13 +0300

    Invoke

--- My project/Assets/AScripts/DamageEnemy.cs
+++ My project/Assets/AScripts/DamageEnemy.cs
@@ -6,27 +6,17 @@ public class DamageEnemy : MonoBehaviour
 {
     [SerializeField] float objDamage;
     private bool hasAttacked;
-    private void Start()
-    {
-        hasAttacked = false;
-    }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Enemies" && !hasAttacked)
         {   /// <summary>Use Damage() function from EnemyHealth script when enemy gets hit with a sword</summary>
             other.GetComponent<EnemyHealth>().Damage(objDamage);
-            hasAttacked = true;
-            StartCoroutine(Delay());
+           Invoke("Test",10f);
         }
     }
-
-    IEnumerator Delay()
-    {
-        /// <summary>
-        ///  Delay
-        /// </summary>
-        yield return new WaitForSeconds(0.4f);
-        hasAttacked = false;
+    void Test(){
+        Debug.Log("2");
     }
+ 
 }
